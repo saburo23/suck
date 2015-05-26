5e3117772a16f54ced01fe98488f11c33937d6e8
Xiandong Wang <keneethwang@gmail.com>
2015-05-26 08:56:17 +0800

    devise views and add major to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true, length: {maximum: 100}
+  validates :major, presence: true
 end
